bb0ff731ab4ca007869be9939ee9e4e065c9762c
kritsanapong754-ship-it <kritsanapong754@gmail.com>
2026-05-31 00:09:25 +0700

    Update leaderboard.js

--- leaderboard.js
+++ leaderboard.js
@@ -1,1 +1,56 @@
+import { db } from "./firebase.js";
 
+import {
+collection,
+getDocs
+}
+from
+"https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+export async function showLeaderboard(){
+
+const snap =
+await getDocs(
+collection(db,"users")
+);
+
+const players=[];
+
+snap.forEach(doc=>{
+
+players.push(
+doc.data()
+);
+
+});
+
+players.sort(
+(a,b)=>
+b.clears-a.clears
+);
+
+let text =
+"TOP CLEAR\n\n";
+
+players
+.slice(0,10)
+.forEach((p,i)=>{
+
+text +=
+`${i+1}. ${p.profileName}
+(${p.clears})\n`;
+
+});
+
+alert(text);
+
+}
+
+document
+.getElementById(
+"leaderboardBtn"
+)
+?.addEventListener(
+"click",
+showLeaderboard
+);
